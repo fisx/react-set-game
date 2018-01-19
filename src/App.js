c500001461b968f90f6b8d4f7013df6ddf5fa204
Matthias Fischmann <mf@zerobuzz.net>
2018-01-19 13:47:23 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,131 +1,17 @@
 import React, { Component } from 'react';
-import moment from 'moment';
+import { connect } from 'react-redux';
 import './App.css';
+import {
+  allCards,
+  rndPermute,
+  replenishBoard,
+  toggleSelect,
+  tickStopwatch,
+  readStopwatch,
+} from './Store.js';
 
 
-const allCards = (() => {
-  let shapes   = ["Diamond", "Round", "Squiggle"];
-  let colors   = ["Red", "Green", "Blue"];
-  let fillings = ["FillingEmpty", "Solid", "GrayedOut"];
-  let numbers  = ["One", "Two", "Three"];
-
-  let r = [];
-  for (var shape in shapes)
-    for (var color in colors)
-      for (var filling in fillings)
-        for (var number in numbers)
-          r.push({
-            shape: shapes[shape],
-            color: colors[color],
-            filling: fillings[filling],
-            number: numbers[number]
-          });
-  return r;
-})();
-
-
-const rndPermute = (v_) => {
-  let v = v_.slice();
-  let r = [];
-  while (v.length > 0) {
-    let i = Math.floor(Math.random() * v.length);
-    if (i < v.length - 1) {
-      r.push(v[i]);
-      v[i] = v.pop();
-    } else {
-      r.push(v.pop());
-    }
-  };
-  return r;
-};
-
-
-const checkTriple = (a, b, c) => {
-  let alleq = k => (
-    allCards[a][k] === allCards[b][k] &&
-    allCards[b][k] === allCards[c][k]
-  );
-  let alldiff = k => (
-    allCards[a][k] !== allCards[b][k] &&
-    allCards[a][k] !== allCards[c][k] &&
-    allCards[b][k] !== allCards[c][k]
-  );
-
-  for (let k in allCards[a])
-    if (!(alleq(k) || alldiff(k)))
-      return { result: 'error', msg: 'not a set: ' + k.toString() };
-
-  return { result: 'ok' };
-};
-
-
-const solve = (cards) => {
-  let solutions = [];
-
-  for (let a = 0; a < cards.length; a++) {
-    for (let b = a + 1; b < cards.length; b++) {
-      for (let c = b + 1; c < cards.length; c++) {
-        if (checkTriple(cards[a], cards[b], cards[c]).result === 'ok')
-          solutions.push([a, b, c]);
-      }
-    }
-  }
-
-  return solutions;
-};
-
-
-const replenishBoard = (state_) => {
-  let state = {...state_};
-  state.solutions = solve(state.board);
-
-  while ((state.board.length < 12 || state.solutions.length === 0) && state.deck.length > 0) {
-    state.board.push(state.deck.pop());
-    state.solutions = solve(state.board);
-  }
-
-  return state;
-};
-
-
-const tickStopwatch = (ticks) => {
-  ticks.unshift(moment());
-};
-
-const readStopwatch = (ticks) => {
-  if (ticks.length < 2)
-    return { total: 'n/a', avg: 'n/a', best: 'n/a', worst: 'n/a', history: 'n/a' };
-
-  let diffs = [];
-
-  for (let i = 0; i < ticks.length - 1 /* sic! */; i++) {
-    diffs.push(ticks[i].diff(ticks[i + 1]));
-  }
-
-  let total   = diffs.reduce((a, b) => a + b, 0);
-  let avg     = total / diffs.length;
-  let best    = Math.min.apply(Math, diffs);
-  let worst   = Math.max.apply(Math, diffs);
-  let history = diffs.slice(0, 5);
-
-  let format = (t) => {
-    let m = Math.floor((t / 1000) / 60).toString();
-    let s = Math.floor((t / 1000) % 60).toString();
-    if (s.length === 1) s = '0'+s;
-    return (m + ':' + s + 's');
-  };
-
-  return {
-    total: format(total),
-    avg: format(avg),
-    best: format(best),
-    worst: format(worst),
-    history: history.map(format).toString()
-  }
-};
-
-
-const initState = (juniorMode, showStopwatch) => {
+const initState = (juniorMode, showStopwatch) => {  // TODO: deprecated in favor of redux.
   let wantThisCard = c => juniorMode ? allCards[c].filling === 'Solid' : true;
   let state = {
     deck: rndPermute(Array.apply(0, Array(allCards.length)).map((x, y) => y).filter(wantThisCard)),
@@ -143,45 +29,6 @@ const initState = (juniorMode, showStopwatch) => {
   tickStopwatch(state.stopwatch);
   return (replenishBoard(state));
 };
-
-
-const toggleSelect = (state_, crd0) => {
-  let state = {...state_};
-  let sel = state.selected;
-
-  if (state.board.findIndex(crd1 => crd1 === crd0) === -1  // no such card
-      || state.solutions.length === 0)                     // game over
-    return;
-
-  let kill = sel.findIndex(crd1 => crd1 === crd0);
-  if (kill === -1)
-    sel.push(crd0)
-  else
-    sel.splice(kill, 1);
-
-  if (sel.length === 3) {
-    let result = checkTriple(sel[0], sel[1], sel[2]);
-    if (result.result === 'ok') {
-      tickStopwatch(state.stopwatch);
-      let s = [];
-
-      for (let grab in sel) {
-        let ix0 = state.board.findIndex(crd => crd === sel[grab]);
-        s.push(state.board[ix0]);
-        state.board.splice(ix0, 1);
-      }
-      state.selected = [];
-      state.setsfound.unshift({val: s, thencount: state.solutions.length});
-      state = replenishBoard(state);
-    } else {
-      state.errmsg = result.msg;
-    }
-  } else {
-    state.errmsg = 'select 3 cards';
-  }
-
-  return state;
-}
 
 
 const handleKeyDown = (getState, setState) => e => {
@@ -228,7 +75,7 @@ const handleKeyDown = (getState, setState) => e => {
 };
 
 
-const Card = ({card, svgPath, onClick, selected, isInShowedSolution}) => {
+const Card = ({card, svgPath, onClick, selected, isPartOfHint}) => {
   const fileStem =
     allCards[card].shape + "-" +
     allCards[card].color + "-" +
@@ -239,7 +86,7 @@ const Card = ({card, svgPath, onClick, selected, isInShowedSolution}) => {
 
   const cls = "board-card"
             + (selected ? " selected" : "")
-            + (isInShowedSolution ? " highlight" : "");
+            + (isPartOfHint ? " highlight" : "");
 
   return (
     <div className={cls} onClick={onClick}>
@@ -267,7 +114,7 @@ const Board = ({state, setState}) => {
     return <Card svgPath="svg"
                  card={crd}
                  selected={issel(crd)}
-                 isInShowedSolution={highlight}
+                 isPartOfHint={highlight}
                  onClick={onclck}
     />
   };
@@ -368,4 +215,11 @@ class App extends Component {
   }
 }
 
-export { App, Card, readStopwatch };
+const storeForApp = (state) => state;
+connect(storeForApp)(App);
+
+export {
+  App,
+  Card,
+  readStopwatch
+};
---
+++ src/Store.js
@@ -0,0 +1,218 @@
+//import React, { Component } from 'react';
+import moment from 'moment';
+import { createStore } from 'redux';
+//import { connect } from 'react-redux';
+import './App.css';
+
+
+//////////////////////////////////////////////////////////////////////
+// set game mechanics
+
+const allCards = (() => {
+  let shapes   = ["Diamond", "Round", "Squiggle"];
+  let colors   = ["Red", "Green", "Blue"];
+  let fillings = ["FillingEmpty", "Solid", "GrayedOut"];
+  let numbers  = ["One", "Two", "Three"];
+
+  let r = [];
+  for (var shape in shapes)
+    for (var color in colors)
+      for (var filling in fillings)
+        for (var number in numbers)
+          r.push({
+            shape: shapes[shape],
+            color: colors[color],
+            filling: fillings[filling],
+            number: numbers[number]
+          });
+  return r;
+})();
+
+
+const rndPermute = (v_) => {
+  let v = v_.slice();
+  let r = [];
+  while (v.length > 0) {
+    let i = Math.floor(Math.random() * v.length);
+    if (i < v.length - 1) {
+      r.push(v[i]);
+      v[i] = v.pop();
+    } else {
+      r.push(v.pop());
+    }
+  };
+  return r;
+};
+
+
+const checkTriple = (a, b, c) => {
+  let alleq = k => (
+    allCards[a][k] === allCards[b][k] &&
+    allCards[b][k] === allCards[c][k]
+  );
+  let alldiff = k => (
+    allCards[a][k] !== allCards[b][k] &&
+    allCards[a][k] !== allCards[c][k] &&
+    allCards[b][k] !== allCards[c][k]
+  );
+
+  for (let k in allCards[a])
+    if (!(alleq(k) || alldiff(k)))
+      return { result: 'error', msg: 'not a set: ' + k.toString() };
+
+  return { result: 'ok' };
+};
+
+
+const solve = (cards) => {
+  let solutions = [];
+
+  for (let a = 0; a < cards.length; a++) {
+    for (let b = a + 1; b < cards.length; b++) {
+      for (let c = b + 1; c < cards.length; c++) {
+        if (checkTriple(cards[a], cards[b], cards[c]).result === 'ok')
+          solutions.push([a, b, c]);
+      }
+    }
+  }
+
+  return solutions;
+};
+
+
+const replenishBoard = (state_) => {  // TODO: refactor.
+  let state = {...state_};
+  state.solutions = solve(state.board);
+
+  while ((state.board.length < 12 || state.solutions.length === 0) && state.deck.length > 0) {
+    state.board.push(state.deck.pop());
+    state.solutions = solve(state.board);
+  }
+
+  return state;
+};
+
+
+const toggleSelect = (state_, crd0) => {  // TODO: refactor?
+  let state = {...state_};
+  let sel = state.selected;
+
+  if (state.board.findIndex(crd1 => crd1 === crd0) === -1  // no such card
+      || state.solutions.length === 0)                     // game over
+    return;
+
+  let kill = sel.findIndex(crd1 => crd1 === crd0);
+  if (kill === -1)
+    sel.push(crd0)
+  else
+    sel.splice(kill, 1);
+
+  if (sel.length === 3) {
+    let result = checkTriple(sel[0], sel[1], sel[2]);
+    if (result.result === 'ok') {
+      tickStopwatch(state.stopwatch);
+      let s = [];
+
+      for (let grab in sel) {
+        let ix0 = state.board.findIndex(crd => crd === sel[grab]);
+        s.push(state.board[ix0]);
+        state.board.splice(ix0, 1);
+      }
+      state.selected = [];
+      state.setsfound.unshift({val: s, thencount: state.solutions.length});
+      state = replenishBoard(state);
+    } else {
+      state.errmsg = result.msg;
+    }
+  } else {
+    state.errmsg = 'select 3 cards';
+  }
+
+  return state;
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// timer
+
+const tickStopwatch = (ticks) => {
+  ticks.unshift(moment());
+};
+
+const readStopwatch = (ticks) => {
+  if (ticks.length < 2)
+    return { total: 'n/a', avg: 'n/a', best: 'n/a', worst: 'n/a', history: 'n/a' };
+
+  let diffs = [];
+
+  for (let i = 0; i < ticks.length - 1 /* sic! */; i++) {
+    diffs.push(ticks[i].diff(ticks[i + 1]));
+  }
+
+  let total   = diffs.reduce((a, b) => a + b, 0);
+  let avg     = total / diffs.length;
+  let best    = Math.min.apply(Math, diffs);
+  let worst   = Math.max.apply(Math, diffs);
+  let history = diffs.slice(0, 5);
+
+  let format = (t) => {
+    let m = Math.floor((t / 1000) / 60).toString();
+    let s = Math.floor((t / 1000) % 60).toString();
+    if (s.length === 1) s = '0'+s;
+    return (m + ':' + s + 's');
+  };
+
+  return {
+    total: format(total),
+    avg: format(avg),
+    best: format(best),
+    worst: format(worst),
+    history: history.map(format).toString()
+  }
+};
+
+
+//////////////////////////////////////////////////////////////////////
+// store construction
+
+const initialState = {
+  config: {
+    juniorMode: false,
+    showSolution: -1
+  },
+  stopwatch: {
+    timestamps: [],
+    showStopwatch: true
+  },
+  game: {
+    deck: Array.apply(0, Array(allCards.length)),
+    setsfound: [],
+    board: [],
+    selected: [],
+    errmsg: 'select 3 cards',
+    solutions: []
+  }
+};
+
+const reducer = (state = initialState, action) => state;
+
+const store = createStore(reducer);  // combineReducers({deck: ...})
+
+
+//////////////////////////////////////////////////////////////////////
+// exports
+
+export {
+  allCards,
+  rndPermute,
+  checkTriple,
+  solve,
+  replenishBoard,
+  toggleSelect,
+
+  tickStopwatch,
+  readStopwatch,
+
+  initialState,
+  store
+};
